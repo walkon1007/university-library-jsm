feba4210d8071b722907c21f76b18d2390b259fd
Hongyu Wang <walkon107@gmail.com>
2025-01-29 18:40:49 -0600

    modify db to neon

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -1,11 +1,13 @@
 import config from "@/lib/config"
-// import { drizzle } from 'drizzle-orm/neon-http';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from 'drizzle-orm/neon-http';
+// import { drizzle } from 'drizzle-orm/node-postgres';
 import { neon } from "@neondatabase/serverless"
 
-// const sql = neon(config.env.databaseUrl);
-import { Pool } from "pg";
-const pool = new Pool({
-    connectionString: config.env.databaseUrl,
-});
-export const db = drizzle({ client: pool });
+const sql = neon(config.env.databaseUrl);
+export const db = drizzle({ client: sql})
+
+// import { Pool } from "pg";
+// const pool = new Pool({ connectionString: config.env.databaseUrl,});
+//
+//
+// export const db = drizzle({ client: pool });
